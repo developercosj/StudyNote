9aa4c47c453383dc6cecac05b602a4fb1ff0566f
이수정 <developercosj@gmail.com>
2024-07-29 23:17:37 +0900

    CodingTest: 최솟값 만들기 - 버블정렬

---
+++ CodingTest/level1/최솟값 만들기 - 버블정렬.java
@@ -0,0 +1,51 @@
+class Scratch {
+    public static void main(String[] args) {
+        int []A = {1,4,2};
+        int []B = {5,4,4};
+
+        solution(A, B);
+    }
+    public static int solution(int[] A, int[] B)
+    {
+        int answer = 0;
+
+        // 첫번째 배열은 오름차순 정렬 (버블정렬 연습)
+        for (int i = 0; i < A.length - 1; i++) {
+            for (int j = 0; j < A.length - 1 - i; j++) {
+                if (A[j] > A[j + 1]) {
+                    int temp = A[j];
+                    A[j] = A[j + 1];
+                    A[j + 1] = temp;
+
+                }
+
+            }
+
+        }
+
+        // 두번째 배열은 내림차순
+        for (int i = 0; i < B.length - 1 ; i++) {
+            for (int j = 0; j < B.length -1 - i; j++) {
+                if (B[j] < B[j + 1]) {
+                    int temp = B[j];
+                    B[j] = B[j + 1];
+                    B[j + 1] = temp;
+
+                }
+
+            }
+
+        }
+
+        // 두개의 배열을 순차적으로 곱할것
+
+        for (int i = 0; i < A.length ; i++) {
+            answer = answer + A[i] * B[i];
+
+        }
+
+
+        return answer;
+    }
+
+}
